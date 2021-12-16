f2dc2161e2fece71d221d970173572e83fd602cb
natriant <nataliatriantafyllou@gmail.com>
2021-12-16 15:34:02 +0100

    general update

---
+++ madx_studies/match_octupoles/job003_match_octupoles_to_detuningCoefficients.py
@@ -0,0 +1,47 @@
+from cpymad.madx import Madx
+
+# MAD-X parameters dictionary
+madx_settings = {'QH':26.13, 'QV':26.18, 'QPH':0.0, 'QPV':1.0}
+seq_name = 'sps'
+harmonic_number = 4620
+
+mad = Madx()
+mad.options.echo = False
+mad.options.info = False
+mad.warn = False
+mad.chdir('./madx')
+mad.call('sps_thin_crabcavity.madx')
+
+for parameter in madx_settings:
+    setting = madx_settings[parameter]
+    mad.input(f'{parameter} = {setting};')
+
+mad.use(seq_name)
+
+# Include b3b5b7 in MBA and MBB
+#mad.call('./sps/cmd/sps_setMultipoles_upto7.cmd')
+#mad.input('exec, set_Multipoles_270GeV;')
+#mad.call('./sps/cmd/sps_assignMultipoles_upto7.cmd')
+#mad.input('exec, AssignMultipoles;')
+
+
+# Tune and Chromaticity matching
+mad.call('./sps/cmd/sps_matching.cmd')
+mad.input('exec, SPS_matchtunes(QH, QV);')
+mad.input('exec, SPS_setchroma_Q26(QPH, QPV);')
+mad.input('acta.31637, harmon=%d;'%harmonic_number)
+mad.input('exec, match_chroma(QPH ,QPV);')
+
+# Match the octupoles
+print('Start octupoles matching')
+ayy_val, axy_val = 20000.0, 0.0
+mad.input(f'exec, match_octupoles({ayy_val}, {axy_val});') # use this line, if the input is the ayy, axy coefficients and then matching to klof, klod
+
+
+table_list = list(mad.table) # list of existing table names
+print(table_list[-1])
+
+my_table = mad.table.matching_results # get table as dict like object
+print(my_table.klof)
+print(my_table.axx)
+
--- match_octupoles/README.md
+++
@@ -1,1 +0,0 @@
-The file job003_match_octupoles_to_detuningCoefficients.py is the same that can be found in the directory ../optics_checks/
